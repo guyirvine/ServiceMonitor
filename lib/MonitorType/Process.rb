35d2433e3774d8374923a66a18dbffe94a6b5b51
Ryan French <RyanFrench@users.noreply.github.com>
2013-11-20 16:10:26 +1300

    Better handling of incorrect input strings

--- lib/MonitorType/Process.rb
+++ lib/MonitorType/Process.rb
@@ -5,7 +5,7 @@ class MonitorType_Process<MonitorType_Threshold
 
 	def sanitise
 		#Ensure that the process name contains a single character surrounded by square brackets
-		@process_name = @process_name.insert(0,'[').insert(2,']') unless @process_name =~ /^.*\[.\].*/
+		@process_name = @process_name.insert(0,'[').insert(2,']') unless @process_name =~ /^.*\[.+\].*/
 	end
 
 	def initialize(params)
@@ -18,7 +18,15 @@ class MonitorType_Process<MonitorType_Threshold
 		end
 		@process_name = params[:process_name]
 
-		puts "*** Min value will be ignored, setting to 1" unless params[:min].nil?
+		puts "#{@process_name}", "result: #{(@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0}"
+
+		unless (@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0 then
+			puts "*** Process Name parameter doest not match the required pattern, #{@process_name}"
+			puts "*** :process_name => <plain string, or a string with one or more characters enclosed in square brackets, i.e. 'foo', '[f]oo' or '[foo]'>"
+			abort
+		end
+
+		puts "*** Min value will be ignored, setting to 1" unless (params[:min].nil? || params[:min] == 1)
 		@min = 1
 
 		puts "*** Max value will be ignored, setting to nil" unless params[:max].nil?
@@ -33,7 +41,7 @@ class MonitorType_Process<MonitorType_Threshold
 	end
 
 	def getValue
-		return `ps aux | grep #{process_name}`.length
+		return `ps aux | grep #{@process_name}`.length
 	end
 
 end
