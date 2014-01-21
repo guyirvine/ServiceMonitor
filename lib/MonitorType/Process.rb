b919890ec5998a01113b566883c6772fb46a7384
Ryan French <rfrench@lic.co.nz>
2014-01-22 11:48:33 +1300

    Changes to the Process monitor to use the same structure as the other monitors. Added unit tests for the Process monitor

--- lib/MonitorType/Process.rb
+++ lib/MonitorType/Process.rb
@@ -1,48 +1,54 @@
 require "MonitorType/Threshold"
+require "helper_functions"
+
+InvalidProcessNameError = Class.new(StandardError)
 
 #A class for checking if a Process is running in Unix based systems
 class MonitorType_Process<MonitorType_Threshold
+
+  #Extract parameters
+  #
+  # @param [String] :process_name THe name of the process to monitor
+  def extractParams
+  	if @params[:process_name].nil? then
+  		string = "*** Process Name parameter missing, process_name\n"
+  		string = "#{string}*** :process_name => <name of the process to be monitored>"
+  		raise MonitorTypeParameterMissingError.new(string)
+  	end
+  	@process_name = @params[:process_name]
+
+  	log "#{@process_name}", "result: #{(@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0}"
+
+  	unless (@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0 then
+  		string = "*** Process Name parameter doest not match the required pattern, #{@process_name}\n"
+  		string = "#{string}*** :process_name => <plain string, or a string with one or more characters enclosed in square brackets, i.e. 'foo', '[f]oo' or '[foo]'>"
+  		raise InvalidProcessNameError.new(string)
+  	end
+
+  	log "*** Min value will be ignored, setting to 1" unless (params[:min].nil? || params[:min] == 0)
+  	@min = 1
+
+  	log "*** Max value will be ignored, setting to nil" unless params[:max].nil?
+  	@max = nil
+
+  	@context_sentence = "Checking that process is running, #{@process_name}"
+  end
+
+  def setup
+  	self.sanitise
+  rescue MonitorTypeExceptionHandled => e
+  	puts e.message
+  	abort
+  end
 
 	def sanitise
 		#Ensure that the process name contains a single character surrounded by square brackets
 		@process_name = @process_name.insert(0,'[').insert(2,']') unless @process_name =~ /^.*\[.+\].*/
 	end
 
-	def initialize(params)
-		super(params)
-
-		if params[:process_name].nil? then
-			puts "*** Process Name parameter missing, process_name"
-			puts "*** :process_name => <name of the process to be monitored>"
-			abort
-		end
-		@process_name = params[:process_name]
-
-		puts "#{@process_name}", "result: #{(@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0}"
-
-		unless (@process_name =~ /^(.*\[{1}.+\]{1}.*)$|^(\w+)$/) == 0 then
-			puts "*** Process Name parameter doest not match the required pattern, #{@process_name}"
-			puts "*** :process_name => <plain string, or a string with one or more characters enclosed in square brackets, i.e. 'foo', '[f]oo' or '[foo]'>"
-			abort
-		end
-
-		puts "*** Min value will be ignored, setting to 1" unless (params[:min].nil? || params[:min] == 0)
-		@min = 1
-
-		puts "*** Max value will be ignored, setting to nil" unless params[:max].nil?
-		@max = nil
-
-		@context_sentence = "Checking that process is running, #{@process_name}"
-
-		self.sanitise
-	rescue MonitorTypeExceptionHandled => e
-		puts e.message
-		abort
-	end
-
-	def getValue
-		return `ps aux | grep #{@process_name}`.length
-	end
+  def getValue
+  	return `ps aux | grep #{@process_name}`.length
+  end
 
 end
 
---
+++ test/test_MonitorType_Process.rb
@@ -0,0 +1,35 @@
+require 'test/unit'
+require './lib/MonitorType/Process.rb'
+require "helper_functions"
+
+
+class MonitorTypeProcessTest < Test::Unit::TestCase
+
+	def test_MustHaveProcessName
+        test = Test_MonitorType.new( :name=>'Bob', :process_name=>"test", :email=>['rfrench@driven-monkey.com'], :email_sender=>['rfrench@driven-monkey.com'] ).extractParams
+
+        exception_raised = false
+        begin
+            test = MonitorType_Process.new( :name=>'Bob', :email=>['rfrench@driven-monkey.com'], :email_sender=>['rfrench@driven-monkey.com'] ).extractParams
+            rescue MonitorTypeParameterMissingError=>e
+            exception_raised = true
+        end
+
+		assert_equal true, exception_raised
+	end
+
+    def test_MustBeValidProcessName
+        test = Test_MonitorType.new( :name=>'Bob', :process_name=>"test", :email=>['rfrench@driven-monkey.com'], :email_sender=>['rfrench@driven-monkey.com'] ).extractParams
+
+        exception_raised = false
+        begin
+            test = MonitorType_Process.new( :name=>'Bob', :process_name=>"#test", :email=>['rfrench@driven-monkey.com'], :email_sender=>['rfrench@driven-monkey.com'] ).extractParams
+            rescue InvalidProcessNameError=>e
+            exception_raised = true
+        end
+
+        assert_equal true, exception_raised
+    end
+
+
+end
